312eda0b1f3d8e507c7b8f948737cf8a8bbf4072
jseparovic1 <jurica.separovic@gmail.com>
2019-11-01 12:01:30 +0100

    Add monolog

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Phpro\ApiProblemBundle\ApiProblemBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
