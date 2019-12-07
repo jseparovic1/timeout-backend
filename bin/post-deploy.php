aac62d092721495c2609ec002932c25c618c8c1a
jseparovic1 <jurica.separovic@gmail.com>
2019-12-07 19:00:00 +0100

    bin

--- bin/post-deploy.php
+++ bin/post-deploy.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 chdir(dirname(__DIR__));
 
 system('touch .env');
-system('chmod 755 bin/console');
+system('chmod 777 bin/console');
 system('composer dump-env prod');
-system('php bin/console cache:clear');
-system('composer timeout:install');
+system('bin/console cache:clear');
+//system('composer timeout:install');
+
+system('bin/console doctrine:database:create --if-not-exists');
+system('bin/console doctrine:schema:update --force');
+system('bin/console doctrine:fixtures:load -n');
