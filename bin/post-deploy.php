d2f98ac248fd8286e74d074de2f4888a68de9cb8
jseparovic1 <jurica.separovic@gmail.com>
2019-12-07 18:48:53 +0100

    change console permission.

--- bin/post-deploy.php
+++ bin/post-deploy.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 chdir(dirname(__DIR__));
 
 system('touch .env');
+system('chmod 755 bin/console');
 system('composer dump-env prod');
 system('php bin/console cache:clear');
 system('composer timeout:install');
