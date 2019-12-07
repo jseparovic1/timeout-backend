9bbba5ddf8ada33491e551ac257f4c2a4851627d
jseparovic1 <jurica.separovic@gmail.com>
2019-12-07 18:48:10 +0100

    add cache clear.

--- bin/post-deploy.php
+++ bin/post-deploy.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 chdir(dirname(__DIR__));
 
+system('touch .env');
 system('composer dump-env prod');
 system('php bin/console cache:clear');
 system('composer timeout:install');
