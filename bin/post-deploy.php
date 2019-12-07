10edf521d44633156cf5225236fbecfa64e898e3
jseparovic1 <jurica.separovic@gmail.com>
2019-12-07 23:04:31 +0100

    remove no debug

--- bin/post-deploy.php
+++ bin/post-deploy.php
@@ -7,7 +7,7 @@ chdir(dirname(__DIR__));
 //system('touch .env');
 //system('composer dump-env prod');
 system('export APP_ENV=prod');
-system('php bin/console cache:clear --no-debug');
+system('php bin/console cache:clear');
 system('php bin/console assets:install');
 system('php bin/console doctrine:database:create --if-not-exists');
 system('php bin/console doctrine:schema:update --force');
