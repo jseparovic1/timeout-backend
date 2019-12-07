63dd9dbf2bec4b3aae40a81195095140c62ba3c0
jseparovic1 <jurica.separovic@gmail.com>
2019-12-07 23:01:10 +0100

    add manual install

--- bin/post-deploy.php
+++ bin/post-deploy.php
@@ -6,8 +6,8 @@ chdir(dirname(__DIR__));
 
 system('touch .env');
 system('composer dump-env prod');
+system('php bin/console cache:clear --env=prod --no-debug');
 system('php bin/console assets:install');
-system('php bin/console cache:clear');
 system('php bin/console doctrine:database:create --if-not-exists');
 system('php bin/console doctrine:schema:update --force');
 system('php bin/console doctrine:fixtures:load -n');
