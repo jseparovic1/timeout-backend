c7d44197a6313c63c81e2b035539ffec3b2cde28
jseparovic1 <jurica.separovic@gmail.com>
2019-12-07 19:01:13 +0100

    commit

--- bin/post-deploy.php
+++ bin/post-deploy.php
@@ -7,9 +7,9 @@ chdir(dirname(__DIR__));
 system('touch .env');
 system('chmod 777 bin/console');
 system('composer dump-env prod');
-system('bin/console cache:clear');
+system('php bin/console cache:clear');
 //system('composer timeout:install');
 
-system('bin/console doctrine:database:create --if-not-exists');
-system('bin/console doctrine:schema:update --force');
-system('bin/console doctrine:fixtures:load -n');
+system('php bin/console doctrine:database:create --if-not-exists');
+system('php bin/console doctrine:schema:update --force');
+system('php bin/console doctrine:fixtures:load -n');
