c1170a516bddbfddb4b4330e3588539ee05ecd85
jseparovic1 <jurica.separovic@gmail.com>
2019-12-07 19:02:41 +0100

    add install.

--- bin/post-deploy.php
+++ bin/post-deploy.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 chdir(dirname(__DIR__));
 
 system('touch .env');
-system('chmod 777 bin/console');
 system('composer dump-env prod');
+system('composer install');
 system('php bin/console cache:clear');
-//system('composer timeout:install');
-
 system('php bin/console doctrine:database:create --if-not-exists');
 system('php bin/console doctrine:schema:update --force');
 system('php bin/console doctrine:fixtures:load -n');
