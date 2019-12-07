439a3ce1939e67a9d8f68bb7bf6d4326a06b07aa
jseparovic1 <jurica.separovic@gmail.com>
2019-12-07 23:03:08 +0100

    Export prod.

--- bin/post-deploy.php
+++ bin/post-deploy.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 chdir(dirname(__DIR__));
 
 //system('touch .env');
-system('composer dump-env prod');
-system('php bin/console cache:clear --env=prod --no-debug');
+//system('composer dump-env prod');
+system('export APP_ENV=prod');
+system('php bin/console cache:clear --no-debug');
 system('php bin/console assets:install');
 system('php bin/console doctrine:database:create --if-not-exists');
 system('php bin/console doctrine:schema:update --force');
